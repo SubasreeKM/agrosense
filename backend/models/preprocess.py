7d5bf01e6c9d01c14f06960c6bfadc2938a86c61
SubasreeKM <2216056@saec.ac.in>
2025-12-07 18:25:20 +0530

    Update preprocess_image to normalize pixel values
    Normalize image pixel values for MobileNetV2 compatibility.

--- backend/models/preprocess.py
+++ backend/models/preprocess.py
@@ -5,5 +5,6 @@ def preprocess_image(path):
     img = cv2.imread(path)
     img = cv2.resize(img, (224, 224))
     img = img.astype(np.float32)
+    img = img / 127.5 - 1      # REQUIRED FOR MobileNetV2
     img = np.expand_dims(img, axis=0)
     return img
